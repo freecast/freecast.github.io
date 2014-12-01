freecast/freecast.github.io

commit 9e8f11f39a4dd73ef68d361232601c7e84895f95

make AI smarter
pick and move according to the floowing priorities:
1. kill other player
2. arrive home
3. get out of the base
4. jump or fly
5. try current pawn

ludo/js/player.js
@@ -196,9 +196,87 @@ Player.prototype.prevPawn = function () {
 	return true;
 }
 
+function getNextPos(pos, diceValue, arrivePos) {
+	var nextPos = pos + diceValue;
+	if (nextPos > arrivePos)
+		nextPos = arrivePos - (nextPos - arrivePos);
+	return nextPos;
+}
+
 Player.prototype.selectPawnAndMove = function(diceValue) {
-	// TODO: simply select a pawn for diceValue
-	// if current pawn is OK, that's it
+	if (this.isTimeOut) {
+		this.timeOutSelectAndMove(diceValue);
+		return;
+	}
+	// 1. kill other player?
+	var i = 0, p, nextPos, dstField;
+	while (p = this.pawns[i]) {
+		if (p.position >= 0) {
+			nextPos = getNextPos(p.position, diceValue, this.arrivePosition);
+			dstField = this.board.getField(this.path[nextPos]);
+			if (checkKill(this, dstField)) {
+				this.blur();
+				this.currentPawn = i;
+				this.focus();
+				this.move(diceValue, p);
+				return;
+			}
+		}
+		i++;
+	}
+	// 2. arrive home
+	i = 0;
+	while (p = this.pawns[i]) {
+		if (p.position + diceValue === this.arrivePosition) {
+			this.blur();
+			this.currentPawn = i;
+			this.focus();
+			this.move(diceValue, p);
+			return;
+		}
+		i++;
+	}
+	// 3. move out of base
+	i = 0;
+	if (diceValue === 6) {
+		while (p = this.pawns[i]) {
+			if (p.position === -1) {
+				this.blur();
+				this.currentPawn = i;
+				this.focus();
+				this.move(diceValue, p);
+				return;
+			}
+			i++;
+		}
+	}
+	// 4. fly or jump
+	i = 0;
+	while (p = this.pawns[i]) {
+		if (p.position >= 0) {
+			nextPos = getNextPos(p.position, diceValue, this.arrivePosition);
+			dstField = this.board.getField(this.path[nextPos]);
+			if (dstField.color === this.color &&
+					(dstField.action === ACTION.JUMP ||
+					 dstField.action === ACTION.FLIGHT)) {
+				this.blur();
+				this.currentPawn = i;
+				this.focus();
+				this.move(diceValue, p);
+				return;
+			}
+		}
+		i++;
+	}
+	// 5. move the current pawn
+	p = this.pawns[this.currentPawn];
+	if (p.position >= 0)
+		this.move(diceValue, p);
+}
+
+Player.prototype.timeOutSelectAndMove = function(diceValue) {
+	// simply select a pawn for diceValue in case of timeout.
+	// If current pawn is OK to move with diceValue, that's it.
 	// otherwise do some simple search
 
 	// 1. current pawn out of base
@@ -328,13 +406,6 @@ Player.prototype.move = function (distance, pawn) {
 					(nextPos - this.arrivePosition);
 	dest = this.path[nextPos];
 	destField = this.board.getField(dest);
-
-	function checkKill(player, destF) {
-		var destP = destF.getPawns();
-		if (destP.length > 0 && destP[0].player !== player)
-			return true;
-		return false
-	};
 
 	if (nextPos < this.arrivePosition) {
 		if (checkKill(this, destField)) {
@@ -528,4 +599,11 @@ Player.prototype.reset = function () {
 	this.currentPawn = 0;
 
 	this.numArrived = 0;
+};
+
+function checkKill(player, destF) {
+	var destP = destF.getPawns();
+	if (destP.length > 0 && destP[0].player !== player)
+		return true;
+	return false
 };

commit 97bdb1fa5c55e5091a81cacd941ad6ed3283b8bf

fix game stuck issue caused by commit 9e8f11f3
should pick up a pawn to move

ludo/js/player.js
@@ -211,7 +211,7 @@ Player.prototype.selectPawnAndMove = function(diceValue) {
 	// 1. kill other player?
 	var i = 0, p, nextPos, dstField;
 	while (p = this.pawns[i]) {
-		if (p.position >= 0) {
+		if (p.position >= 0 && p.isArrived === false) {
 			nextPos = getNextPos(p.position, diceValue, this.arrivePosition);
 			dstField = this.board.getField(this.path[nextPos]);
 			if (checkKill(this, dstField)) {
@@ -219,6 +219,7 @@ Player.prototype.selectPawnAndMove = function(diceValue) {
 				this.currentPawn = i;
 				this.focus();
 				this.move(diceValue, p);
+				console.log("selectPawnAndMove finished with kill-other");
 				return;
 			}
 		}
@@ -232,6 +233,7 @@ Player.prototype.selectPawnAndMove = function(diceValue) {
 			this.currentPawn = i;
 			this.focus();
 			this.move(diceValue, p);
+			console.log("selectPawnAndMove finished with arrive-home");
 			return;
 		}
 		i++;
@@ -245,6 +247,7 @@ Player.prototype.selectPawnAndMove = function(diceValue) {
 				this.currentPawn = i;
 				this.focus();
 				this.move(diceValue, p);
+				console.log("selectPawnAndMove finished with out-of-base");
 				return;
 			}
 			i++;
@@ -253,7 +256,7 @@ Player.prototype.selectPawnAndMove = function(diceValue) {
 	// 4. fly or jump
 	i = 0;
 	while (p = this.pawns[i]) {
-		if (p.position >= 0) {
+		if (p.position >= 0 && p.isArrived === false) {
 			nextPos = getNextPos(p.position, diceValue, this.arrivePosition);
 			dstField = this.board.getField(this.path[nextPos]);
 			if (dstField.color === this.color &&
@@ -263,15 +266,30 @@ Player.prototype.selectPawnAndMove = function(diceValue) {
 				this.currentPawn = i;
 				this.focus();
 				this.move(diceValue, p);
+				console.log("selectPawnAndMove finished with fly/jump");
 				return;
 			}
 		}
 		i++;
 	}
 	// 5. move the current pawn
-	p = this.pawns[this.currentPawn];
-	if (p.position >= 0)
-		this.move(diceValue, p);
+	i = 0;
+	var index = this.currentPawn;
+	while (i < 4) {
+		p = this.pawns[index];
+		if (p.position >= 0 && p.isArrived === false) {
+			this.blur();
+			this.currentPawn = i;
+			this.focus();
+			this.move(diceValue, p);
+			console.log("selectPawnAndMove finished with %d", index);
+			return;
+		}
+		i++;
+		index++;
+		if (index === 4)
+			index = 0;
+	}
 }
 
 Player.prototype.timeOutSelectAndMove = function(diceValue) {
@@ -290,7 +308,9 @@ Player.prototype.timeOutSelectAndMove = function(diceValue) {
 	var i = 0, pawn;
 	while (pawn = this.pawns[i]) {
 		if (pawn.isArrived == false && pawn.position >= 0) {
+			this.blur();
 			this.currentPawn = i;
+			this.focus();
 			this.move(diceValue, pawn);
 			return;
 		}
@@ -302,7 +322,9 @@ Player.prototype.timeOutSelectAndMove = function(diceValue) {
 		i = this.currentPawn;
 		while (pawn = this.pawns[i]) {
 			if (pawn.isArrived == false) {
+				this.blur();
 				this.currentPawn = i;
+				this.focus();
 				this.move(diceValue, pawn);
 				return;
 			}